9fb8235661a643114e1636fa7b6c4c20aed29adc
Beaver <hudim@mail.ru>
2017-09-09 19:45:32 +0300

    + change column generate type

--- src/main/java/com/khudim/dao/entity/Content.java
+++ src/main/java/com/khudim/dao/entity/Content.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
 @Data
 public class Content implements Serializable{
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private long id;
     private byte[] image;
     private String path;
--- src/main/java/com/khudim/dao/entity/Video.java
+++ src/main/java/com/khudim/dao/entity/Video.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
 @Data
 public class Video implements Serializable {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private long id;
     private String name;
     private long date;
